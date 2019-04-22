9f7c1d727628d4e879f773ae726037e42754f435
guptahardik17 <guptahardik17@gmail.com>
2019-04-22 21:52:38 +0530

    Change

--- backend/config/dbconn.js
+++ backend/config/dbconn.js
@@ -2,10 +2,6 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
 	supportBigNumbers: true,
 	bigNumberStrings: true,
-	// host: 'localhost',
-  // user: 'himaekuk_miclabu',
-  // password : 'Labh2Mic$',
-  // database : 'himaekuk_miclab'
 	host: 'localhost',
   user: 'root',
   password : 'root',
